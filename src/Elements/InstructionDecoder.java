b9f256a8ea9e2c6478b7a0f097c1ffe39d649891
abbaru <37040801+abbaru@users.noreply.github.com>
2020-04-09 13:03:40 +0200

    InstructionDecoder changed int to hex

--- src/Elements/InstructionDecoder.java
+++ src/Elements/InstructionDecoder.java
@@ -16,92 +16,92 @@ public class InstructionDecoder extends Element {
         inputCommand = command;
 
         //Mask all 14 Bits. If there are more than 14 Bits, they are cut away
-        command = command & 16383;
+        command = command & 0x3FFF;
 
         //Mask Bit 14 and 13
-        int tmp = command & 12288;
+        int tmp = command & 0x3000;
 
         //Bit 14,13: 00
-        if (tmp == 0) {
-            tmp = command & 3840;
+        if (tmp == 0x0) {
+            tmp = command & 0x3000;
 
-            if (tmp == 0) {
+            if (tmp == 0x0) {
                 //Bits 4,3,2,1;
-                tmp = command & 15;
-                if (tmp == 0) {
+                tmp = command & 0xF;
+                if (tmp == 0x0) {
                     //Bits 14,13,12,11,10,9,8
                     //Commands: NOP
-                    decodedCommand = command & 16256;
+                    decodedCommand = command & 0x3F80;
                 } else {
                     //Bit 8
-                    tmp = command & 128;
-                    if (tmp == 0) {
+                    tmp = command & 0x80;
+                    if (tmp == 0x0) {
                         //All Bits
                         //Control Commands: RETURN, SLEEP, ....
                         decodedCommand = command;
                     } else {
                         //Bits 14,13,12,11,10,9,8
                         //Commands: MOVWF
-                        decodedCommand = command & 16256;
+                        decodedCommand = command & 0x3F80;
                     }
                 }
-            } else if (tmp == 256) {
+            } else if (tmp == 0x100) {
                 //Bits 14,13,12,11,10,9,8
                 //Commands: CLRW, CLRF
-                decodedCommand = command & 16256;
+                decodedCommand = command & 0x3F80;
             } else {
                 //Bits 14,13,12,11,10,9
                 //Commands: SUBWF, DECF, IORWF, .........
-                decodedCommand = command & 16128;
+                decodedCommand = command & 0x3F00;
             }
 
         }
 
         //Bit 14,13: 01
-        else if (tmp == 4096) {
+        else if (tmp == 0x1000) {
             //Bits 14,13,12,11
             //Commands: BCF, BSF, BTFSC, BTFSS
-            decodedCommand = command & 15360;
+            decodedCommand = command & 0x3C00;
         }
 
         //Bit 14,13: 10
-        else if (tmp == 8192) {
+        else if (tmp == 0x2000) {
             //Bits 14,13,12
             //Commands: CALL, GOTO
-            decodedCommand = command & 14336;
+            decodedCommand = command & 0x3800;
         }
 
         //Bit 14,13: 11
-        else if (tmp == 12288) {
+        else if (tmp == 0x3000) {
             //Bits 12,11
-            tmp = command & 3072;
+            tmp = command & 0xC00;
 
             //Bit 12,11: 00
-            if (tmp == 0) {
+            if (tmp == 0x0) {
                 //Bits 14,13,12,11
                 //Command: MOVLW
-                decodedCommand = command & 15360;
+                decodedCommand = command & 0x3C00;
             }
 
             //Bit 12,11: 01
-            else if (tmp == 1024) {
+            else if (tmp == 0x400) {
                 //Bits 14,13,12,11
                 //Command: RETLW
-                decodedCommand = command & 15360;
+                decodedCommand = command & 0x3C00;
             }
 
             //Bit 12,11: 11
-            else if (tmp == 3072) {
+            else if (tmp == 0xC00) {
                 //Bits 14,13,12,11,10
                 //Commands: SUBLW, ADDLW
-                decodedCommand = command & 15872;
+                decodedCommand = command & 0x3E00;
             }
 
             //Bit 12,11: 10
             else {
                 //Bits 14,13,12,11,10,9
                 //Commands: IORLW, ANDLW, XORLW
-                decodedCommand = command & 16128;
+                decodedCommand = command & 0x3F00;
             }
         }
 
